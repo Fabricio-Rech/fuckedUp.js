0944dad87a2d355508d19506e2378d5bf7da9fd4
Fah <80393387+Hollow-Soul@users.noreply.github.com>
2022-07-21 18:20:17 -0300

    Add files via upload

---
+++ fuckedUp.js/bananaCode.js
@@ -0,0 +1,10 @@
+const fruit = ('b' + 'a' + + 'a' + 'a').toLowerCase();
+
+const calc1 = 2+"2"+2;
+
+const calc2 = 2+"2"-2;
+
+
+console.log("const fruit = ", fruit);
+console.log("const calc1 = ", calc1);
+console.log("const calc2 = ", calc2);
